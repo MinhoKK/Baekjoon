b15b38155ea1931efb16b5f69eb88bf516b0e3e1
mino <koho1047@naver.com>
2022-08-22 14:50:23 +0900

    first commit

---
+++ src/그리드/Num1026.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Num1026 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int aArray[] = new int [len];
+        int bArray[] = new int [len];
+
+        for(int i=0; i<len; i++)
+            aArray[i] = sc.nextInt();
+
+
+        for(int i=0; i<len; i++)
+            bArray[i] = sc.nextInt();
+
+        Arrays.sort(aArray);
+        Arrays.sort(bArray);
+        int sum=0;
+
+        for(int i=0; i<len; i++){
+            sum += aArray[i]*bArray[len-i-1];
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/그리드/Num2217.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Num2217 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int lopeWeight[] = new int [num];
+
+        for(int i=0; i<num; i++){
+            lopeWeight[i] = sc.nextInt();
+        }
+
+        Arrays.sort(lopeWeight);
+        int max = 0;
+
+        for(int j=0; j<num; j++){
+            int check = lopeWeight[j]*(num-j);
+            if(check > max){
+                max = check;
+            }
+        }
+        System.out.println(max);
+    }
+}
+
